f0564c8ee27934dfa25b306ea058ad528a898d85
wetlandsmanagements@gmail.com <wetlandsmanagements@gmail.com>
2026-09-20 15:29:55 +0530

    Initial commit: hospital management service

---
+++ src/main/java/com/secure/hms/NewHospitalServiceApplication.java
@@ -0,0 +1,13 @@
+package com.secure.hms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewHospitalServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewHospitalServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/secure/hms/NewHospitalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.secure.hms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewHospitalServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
